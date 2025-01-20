ccdb802cb626c2f8ff56fdc9c003454107a4e41b
Amankos Ansar <zugzwanggg0@gmail.com>
2025-01-20 10:58:33 +0500

    Backend: cleaning up index.js

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,6 @@ dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import passport from "passport";
-import {passport as googlePassport} from "./services/passport.js"
-import jwt from "jsonwebtoken";
-
-
 
 // routes
 import { authRoute } from "./routes/authRoute.js";
@@ -29,24 +25,6 @@ app.use(passport.initialize());
 app.use('/api', authRoute);
 app.use('/api', checkAuth,userRoute);
 app.use('', checkAuth, mediaRoute);
-
-// oauth2
-const JWT_SECRET = process.env.JWT_SECRET || "dev-pass";
-
-app.get('/auth/google', googlePassport.authenticate('google', {scope: ['profile', 'email']}));
-app.get('/auth/google/callback', googlePassport.authenticate('google', {session: false, failureRedirect: `${process.env.FRONTEND_BASE_URL}/login`}), (req,res)=> {
-    if (!req.user) {
-      return res.status(401).json({ message: 'Authentication failed' });
-    }
-    const token = jwt.sign(req.user, JWT_SECRET);
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: 'none'
-    });
-    res.redirect(`${process.env.FRONTEND_BASE_URL}}`)
-})
 
 const PORT = process.env.PORT || 8080;
 
--- backend/routes/mediaRoute.js
+++ backend/routes/mediaRoute.js
@@ -1,7 +1,24 @@
 import {Router} from "express";
 import { connectToInstagram, getStatistics } from "../controllers/mediaController.js";
+import {passport as googlePassport} from "../services/passport.js"
+import jwt from "jsonwebtoken";
 
 export const mediaRoute = Router();
 
 mediaRoute.get('api/getStatistics/:user_id', getStatistics);
 mediaRoute.get('auth/instagram/callback', connectToInstagram);
+
+mediaRoute.get('/auth/google', googlePassport.authenticate('google', {scope: ['profile', 'email']}));
+mediaRoute.get('/auth/google/callback', googlePassport.authenticate('google', {session: false, failureRedirect: `${process.env.FRONTEND_BASE_URL}/login`}), (req,res)=> {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+    const token = jwt.sign(req.user, process.env.JWT_SECRET);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: 'none'
+    });
+    res.redirect(`${process.env.FRONTEND_BASE_URL}}`)
+})
