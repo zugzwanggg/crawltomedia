cad2a1bd31e62ea0c02387eedf0e92034d258a51
Amankos Ansar <zugzwanggg0@gmail.com>
2025-01-26 16:21:02 +0500

    frontend: home changing logic

--- frontend/src/components/LineGraph.tsx
+++ frontend/src/components/LineGraph.tsx
@@ -1,6 +1,4 @@
-import {useState, useEffect} from 'react';
 import { Line } from 'react-chartjs-2';
-import axios from 'axios';
 import { 
   Chart as ChartJS, 
   CategoryScale, 
@@ -11,7 +9,6 @@ import {
   Tooltip,
   Legend
 } from 'chart.js';
-import { useTranslation } from 'react-i18next';
 
 ChartJS.register(
   CategoryScale, 
@@ -23,52 +20,7 @@ ChartJS.register(
   Legend
 )
 
-
-// types
-import { TypeApp } from '../types';
-import { useAppSelector } from '../app/hooks';
-
 const LineGraph = () => {
-  const {t} = useTranslation();
-  const {user} = useAppSelector(state=>state.user);
-  const userId = user?.id;
-
-  const [currentStats, setCurrentStats] = useState('overall');
-  const [userData, setUserData] = useState<TypeApp[]>([]);
-
-  const fetchUserApps = async () => {
-    try {
-
-      const res = await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/apps/${userId}`);
-      setUserData(res.data)
-      
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const getInstaStats =async () => {
-    try {
-
-      const res = await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/getInstaStatistics/${userId}/1`);
-      console.log(res);
-
-    } catch (error) {
-      console.log(error);
-      
-    }
-  }
-
-  getInstaStats();
-
-  useEffect(()=> {
-    fetchUserApps();
-  }, [])
-
-  const handleStatsChange = (name:string) => {
-    setCurrentStats(name);
-  }
-
   const options = {
     responsive: true,
     plugins: {
@@ -97,37 +49,10 @@ const LineGraph = () => {
   };
 
   return (
-    <div className='bg-primaryColor shadow-lg dark:bg-darkPrimaryColor w-full rounded-lg p-8 h-fit'>
-      <ul className="flex items-center gap-2 py-2 overflow-x-auto">
-        <li onClick={()=> handleStatsChange('overall')} className={`
-        ${
-          currentStats == 'overall' 
-          ? 
-          'bg-darkPrimaryColor text-white dark:bg-whiteGray dark:text-darkPrimaryColor border-darkPrimaryColor' 
-          : 
-          ' bg-white dark:bg-grayColor'
-          } px-4 py-2 rounded cursor-pointer shadow`}>
-          {t('all')}
-        </li>
-        {
-          userData.map(app => {
-            return <li onClick={()=> handleStatsChange(app.name)} className={`
-            ${
-              currentStats == app.name
-              ? 
-              'bg-darkPrimaryColor text-white dark:bg-whiteGray dark:text-darkPrimaryColor border-darkPrimaryColor' 
-              : 
-              ' bg-white dark:bg-grayColor'
-              } px-4 py-2 rounded cursor-pointer shadow`}>
-              {app.name}
-            </li>
-            
-          })
-        }
-      </ul>
+    <>
       <h2 className='font-semibold text-2xl mt-10'>Views</h2>
       <Line options={options} data={data} className='max-w-full shadow-lg bg-gray-800 p-6 rounded mt-3'/>
-    </div>
+    </>
   )
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,41 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 import SocMedias from "../components/SocMedias"
 import LineGraph from "../components/LineGraph"
 import DailyStats from "../components/DailyStats"
 
 import { useTranslation } from "react-i18next"
 
+import { TypeApp } from '../types';
+import { useAppSelector } from '../app/hooks';
+
 const Home = () => {
   const {t} = useTranslation();
+  const [currentStats, setCurrentStats] = useState('overall');
+
+  const [userData, setUserData] = useState<TypeApp[]>([]);
+  const {user} = useAppSelector(state=>state.user);
+  const userId = user?.id;
+
+  const fetchUserApps = async () => {
+    try {
+
+      const res = await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/apps/${userId}`);
+      setUserData(res.data)
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=> {
+    fetchUserApps();
+  }, [])
+
+  const handleStatsChange = (name:string) => {
+    setCurrentStats(name);
+  }
 
   return (
     <div className="lg:p-4 dark:text-white">
@@ -14,7 +44,36 @@ const Home = () => {
       </h1>
 
       <div className="flex flex-wrap lg:flex-nowrap px-2 gap-4">
-        <LineGraph/>
+        <div className='bg-primaryColor shadow-lg dark:bg-darkPrimaryColor w-full rounded-lg p-8 h-fit'>
+          <ul className="flex items-center gap-2 py-2 overflow-x-auto">
+            <li onClick={()=> handleStatsChange('overall')} className={`
+            ${
+              currentStats == 'overall' 
+              ? 
+              'bg-darkPrimaryColor text-white dark:bg-whiteGray dark:text-darkPrimaryColor border-darkPrimaryColor' 
+              : 
+              ' bg-white dark:bg-grayColor'
+              } px-4 py-2 rounded cursor-pointer shadow`}>
+              {t('all')}
+            </li>
+            {
+              userData.map(app => {
+                return <li onClick={()=> handleStatsChange(app.name)} className={`
+                ${
+                  currentStats == app.name
+                  ? 
+                  'bg-darkPrimaryColor text-white dark:bg-whiteGray dark:text-darkPrimaryColor border-darkPrimaryColor' 
+                  : 
+                  ' bg-white dark:bg-grayColor'
+                  } px-4 py-2 rounded cursor-pointer shadow`}>
+                  {app.name}
+                </li>
+                
+              })
+            }
+          </ul>
+          <LineGraph/>
+        </div>
         <div className="flex flex-wrap w-full md:flex-row md:flex-nowrap lg:flex-col lg:w-1/3 gap-4">
           <SocMedias/>
           <DailyStats/>
